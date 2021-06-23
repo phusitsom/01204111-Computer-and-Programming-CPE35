328900d93a60c689d44e98e37ad0eb6f26d5d779
Phusit Somboonyingsuk <phusitsom@gmail.com>
2021-06-23 13:49:27 +0700

    Add files

---
+++ Pretest/pretest_light.py
@@ -0,0 +1,15 @@
+height = float(input())
+price = int(input())
+
+def is_corruption(h, p):
+    if (h < 1) & (p < 1000): 
+        print('NO')
+    elif (h > 1) & (h < 4) & (p < 5000): 
+        print('NO')
+    elif (h > 4) & (h < 8) & (p < 30000): 
+        print('NO')
+    elif (h > 8) & (p < 75000): 
+        print('NO')
+    else: print('YES')
+
+is_corruption(height, price)
