c85b26cf8255ef25b0b0e80cacee5a829ba6eaa7
Phusit Somboonyingsuk <phusit.so@ku.th>
2021-07-10 18:09:00 +0700

    Update

--- Lab/L02/07-buzznaldo.py
+++ Lab/L02/07-buzznaldo.py
@@ -1,8 +1,6 @@
 totalMins = int(input('How long have Buzz played ?: ')) #mins
 h, m = divmod(totalMins,60)
-if m > 20: 
-    h += 1
-    m = 0
+if m > 20: h, m = h+1, 0
 if h >= 5: prom = 0.3
 elif h >= 4: prom = 0.2
 elif h >= 2: prom = 0.1
--- Lab/L02_HW/04-date.py
+++ Lab/L02_HW/04-date.py
@@ -1,6 +1,2 @@
-# from datetime import date
-d, m, y = int(input('d: ')), int(input('m: ')), int(input('y: '))
-# date_val = date(y, m, d)
-# print(int(date_val.strftime('%j')))
-if (y%400 == 0 ) or (y%4 == 0) and (y%100 != 0): leap = True
-else: leap = False
+from datetime import date
+print(int(date(*reversed([int(input('d: ')), int(input('m: ')), int(input('y: '))])).strftime('%j')))
