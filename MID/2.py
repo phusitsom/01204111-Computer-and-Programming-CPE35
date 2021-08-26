6c4a92282b0127cea3629f7717a4b70fe99c1f7e
Phusit Somboonyingsuk <phusitsom@gmail.com>
2021-08-26 18:09:15 +0700

    Update MID

--- MID/2.py
+++ MID/2.py
@@ -1,4 +1,3 @@
-import statistics
 perc_to_grade = {
     85 :"A",
     79 :"B+", 
@@ -14,16 +13,16 @@ grade_std_count = {g:0 for g in perc_to_grade.values()}
 
 with open('midTermScore_grp01.csv') as fp:
   data = (lambda x: [dict(zip(x[0].split(','),row.split(','))) for row in x[1:] if 'Midterm' in row])(fp.read().splitlines())
-  
+
 print(len(data))
 
-(lambda scores: print(f"{max(scores)} {statistics.mean(scores):.2f} {statistics.stdev(scores):.2f} {min(scores)}"))([int(row['Score']) for row in data])
+(lambda scores: print(f"{max(scores)} {sum(scores)/len(scores):.2f} {(sum([(score -(sum(scores)/len(scores)))**2 for score in scores])/len(scores))**0.5:.2f} {min(scores)}"))([int(row['Score']) for row in data])
 
 percs = [float(row['Percentage']) for row in data]
 
 for perc in percs:
     for perc_grade in perc_to_grade:
-        if perc >= perc_grade:
+        if perc > perc_grade:
             grade = perc_to_grade[perc_grade]
             grade_std_count[grade] += 1
             break
