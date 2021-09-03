5c4b17b154c68812d22a05f298809e639b469ea8
Phusit Somboonyingsuk <phusitsom@gmail.com>
2021-09-03 12:23:42 +0700

    Better regex L08 04

--- LAB/L08/04-Sequence-draw/04-Sequence-draw.py
+++ LAB/L08/04-Sequence-draw/04-Sequence-draw.py
@@ -2,7 +2,7 @@ import re
 with open(input('File name: ')) as file:
     fr = file.read()
 
-regex_lego = r"[^0-9\n]+[\n]+[^0-9]+|[^0-9\n]+[^0-9]+"
+regex_lego = r"[^0-9\n]+[^0-9]+"
 regex_order = r"[0-9]"
 
 lego = re.findall(regex_lego, fr)
